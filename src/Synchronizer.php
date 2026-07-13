e9eae4beb3ea4e65be3f849d397ef4fd37d3c427
shimomo <yuichi@shimomo.net>
2026-07-13 21:33:02 +0900

    refactor!: reimplement from scratch

---
+++ src/Synchronizer.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BOA\Previews;
+
+use BVP\Scraper\Scraper;
+use Carbon\CarbonImmutable as Carbon;
+use DateTimeInterface;
+
+/**
+ * @author shimomo
+ */
+final class Synchronizer
+{
+    /**
+     * @param \DateTimeInterface|string $date
+     * @return array<array-key, mixed>
+     */
+    public static function sync(DateTimeInterface|string $date = 'today'): array
+    {
+        $date = Carbon::parse($date, 'Asia/Tokyo');
+
+        /** @var array<array-key, array<array-key, array<array-key, array{boats: array<mixed>}>>> $previews */
+        $previews = Scraper::scrapePrograms($date);
+
+        return self::normalize($previews);
+    }
+
+    /**
+     * @param array<array-key, array<array-key, array<array-key, array{boats: array<mixed>}>>> $previews
+     * @return array<array-key, mixed>
+     */
+    private static function normalize(array $previews): array
+    {
+        $newPreviews = [];
+
+        foreach (array_values($previews) as $data) {
+            foreach (array_values($data) as $preview) {
+                $preview['boats'] = isset($preview['boats'])
+                    ? array_values($preview['boats'])
+                    : [];
+
+                $newPreviews[] = $preview;
+            }
+        }
+
+        return $newPreviews;
+    }
+}
